9c5e66a9841d5f521d79fbdef479b5a41a2b8249
Arun Chinnaraj <Arun.Chinnaraj@mea.ey.net>
2017-11-08 12:37:33 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var express = require('Express');
 var app = express();
+app.use(express.static('public'));
 
 var things = require('./routes.js');
 
 //both index.js and things.js should be in same directory
 app.use('/', things);
 
-app.listen(3000);
+app.listen(3000);   
+
+// var express = require('express');
+// var app = express();
+// var num = 0;
+// //First middleware before response is sent
+// app.use('/things',function(req, res, next){
+//    console.log("Started at" + num++);
+//    next();
+// });
+
+// //Route handler
+// app.get('/things', function(req, res, next){
+//    res.send("Middle at "+ num++);
+//    next();
+// });
+
+// app.use('/things', function(req, res){
+//    console.log("Ended at" + num);
+//    num=0;
+// });
+
+// app.listen(3000);
--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path    = require("path");
 
 router.get('/', function(req, res){
-   res.send('GET route on things.');
+//    res.sendFile(path.join(__dirname+'/public/html/index.html'));
+//    res.sendFile(path.join(__dirname+'/public/css/index.css'));
 });
-app.get('/:id', function(req, res){
-    res.send('The id you specified is ' + req.params.id);
+// router.get('/:id', function(req, res){
+//     res.send('The id you specified is ' + req.params.id + typeof(parseInt(req.params.id)));
+//  });
+ router.get('*', function(req, res){
+    res.send('Wrong URL');
  });
 
 //export this router to use in our index.js
